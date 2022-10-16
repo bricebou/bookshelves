206040c428704102688e98987db98ef9931653ee
bricebou <brice@reclic.dev>
2022-10-16 21:33:16 +0200

    chore(frontend): Mise en place de Bootstrap
    Activation du SassLoader de Webpack
    Import de l'ensemble des styles via les fichiers scss
    Préparation à l'appel des scripts

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,21 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+// Bootstrap Scripts
+// import { Alert } from "bootstrap";
+// import { Button } from "bootstrap";
+// import { Carousel } from "bootstrap";
+// import { Collapse } from "bootstrap";
+// import { Dropdown } from "bootstrap";
+// import { Modal } from "bootstrap";
+// import { Offcanvas } from "bootstrap";
+// import { Popover } from "bootstrap";
+// import { ScrollSpy } from "bootstrap";
+// import { Tab } from "bootstrap";
+// import { Toast } from "bootstrap";
+// import { Tooltip } from "bootstrap";
 
 // start the Stimulus application
 import './bootstrap';
