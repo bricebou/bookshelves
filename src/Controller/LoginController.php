e757f5b087c8c151355e853ded515828aa739602
bricebou <brice@reclic.dev>
2022-10-16 23:00:31 +0200

    feat(authentification): Connexion et déconnexion
    Définition des routes /login et /logout
    Formulaire de connextion
    Configuration du bundle Security
    Chaînes de langues

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'bks_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
+    }
+
+    #[Route('/logout', name: 'bks_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+    }
+}
