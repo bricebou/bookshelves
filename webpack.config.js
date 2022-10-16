76ae2f84e63d5a8153ace77e0d88e4c777e9d256
bricebou <brice@reclic.dev>
2022-10-16 21:44:02 +0200

    build(frontend): Webpack FileLoader, PostCSS et PurgeCSS

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,19 @@ Encore
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
+    .copyFiles({
+        from: './assets/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+
     /*
      * ENTRY CONFIG
      *
@@ -72,5 +85,9 @@ Encore
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
 ;
+
+if (Encore.isProduction()) {
+    Encore.enablePostCssLoader();
+}
 
 module.exports = Encore.getWebpackConfig();
