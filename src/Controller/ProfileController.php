ce396b97b2536f357f049ba6e065c9464a30a844
bricebou <brice@reclic.dev>
2022-10-17 16:44:36 +0200

    feat: Mise en place d'une page Profil basique

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/profile', name: 'bks_profile_')]
+class ProfileController extends AbstractController
+{
+    #[Route('/{username}', name: 'view')]
+    public function view(User $user): Response
+    {
+        return $this->render('profile/view.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
