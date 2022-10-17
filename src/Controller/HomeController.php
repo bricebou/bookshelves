5fc51fee406a2c18cd6b69c9d0b3fde8033546fa
bricebou <brice@reclic.dev>
2022-10-17 09:00:16 +0200

    feat(frontend): Intégration de l'authentification
    Ajout du formulaire de login à la page d'accueil
    Modification du header avec liens de (dé)connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'bks_home')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('index.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUsername,
+        ]);
     }
 }
